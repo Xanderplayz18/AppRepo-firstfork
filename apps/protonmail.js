0f214956d0f26236f3a8bc5ced2c63a3b77e036b
Xanderplayz18 <98059420+Xanderplayz18@users.noreply.github.com>
2024-01-12 13:27:48 -0500

    Create protonmail.js

---
+++ apps/protonmail.js
@@ -0,0 +1,56 @@
+export default class ProtonMailApp {
+  meta = {
+    name: 'Proton Mail',
+    description: 'Protect your communications with encrypted email that is private by default.',
+    icon: 'https://raw.githubusercontent.com/Flow-Works/AppRepo/main/assets/discord.svg',
+    pkg: 'proton.mail',
+    version: '1.0.0'
+  }
+
+  async open () {
+    const win = window.wm.createWindow({
+      title: this.meta.name,
+      icon: this.meta.icon,
+      width: 700,
+      height: 500
+    })
+
+    const xor = {
+      randomMax: 100,
+      randomMin: -100,
+
+      encode: (str) => {
+        if (!str) return str
+        return encodeURIComponent(
+          str
+            .toString()
+            .split('')
+            .map((char, ind) =>
+              ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char
+            )
+            .join('')
+        )
+      },
+      decode: (str) => {
+        if (!str) return str
+        const [input, ...search] = str.split('?')
+
+        return (
+          decodeURIComponent(input)
+            .split('')
+            .map((char, ind) =>
+              ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char
+            )
+            .join('') + (search.length ? '?' + search.join('?') : '')
+        )
+      }
+    }
+
+    win.content.style.background = 'var(--base)'
+    win.content.innerHTML = `
+      <iframe src="/service/${xor.encode('https://mail.proton.me/')}" style="width: 100%;height: 100%;border: none;"/>
+    `
+
+    return win
+  }
+}
